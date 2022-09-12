d15fe273604a566ac92c484fe29dfda13b25cacd
alok <alok60541@gmail.com>
2022-09-12 23:12:05 +0530

    basic functions

---
+++ 3_function.js
@@ -0,0 +1,24 @@
+
+
+// let x = myFunction(4,5);
+
+// function myFunction(a,b){
+//     return a*b;
+// }
+// console.log(x);
+
+// function toCelsius(fahrenheit){
+//     return ((5/9) * (fahrenheit - 32));
+// }
+
+// let y;
+// y = document.getElementById("demo").innerHTML = toCelsius;
+
+// console.log(y);
+
+// y = document.getElementById("demo").innerHTML = toCelsius(77);
+
+// console.log(y);
+
+// document.write("jai ho");
+
