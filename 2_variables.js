64c0546793dae56b818bddbf0c7efae40e669667
alok <alok60541@gmail.com>
2022-09-12 23:11:14 +0530

    varibles js

---
+++ 2_variables.js
@@ -0,0 +1,50 @@
+console.log("print karo");
+
+// variables in javascript
+// var , let , const
+
+
+console.log("kaisen baa ho ");
+
+document.getElementById("demo").innerHTML =
+"Hello Alok pehle wala kahan gaya";
+
+var x;
+var y;
+
+x=5;
+y = 6;
+
+let z = x + y;
+console.log(z);
+
+let alok_name = "main hoon alok";
+console.log(alok_name);
+
+let first = "alok " + "kumar";
+console.log(first);
+
+let second = "5" + 3 + 5;
+console.log(second);
+
+let third = 5 + "34" + 6;
+console.log(third);
+
+let $ = "ham hain";
+console.log($);
+
+let $$$$ = "ham bhi hain";
+console.log($$$$);
+
+var fst = 10;
+console.log(fst + " above");
+{
+    var fst = 4;
+    console.log(fst + " in block");
+}
+
+console.log(fst + " outside block");
+
+carName = "volvo";
+var carName;
+console.log(carName);
