4fe519c56bbec80f89edc9ac157a2c2a13384330
alok <alok60541@gmail.com>
2022-09-12 23:14:38 +0530

    prct

---
+++ basic.js
@@ -0,0 +1,13 @@
+
+console.time('Your code took');
+console.log('hello world');
+console.log(4+34);
+console.log(34);
+console.log(true);
+console.warn("This is a warning");
+// console.clear();
+
+console.timeEnd('Your code took');
+
+console.assert(5666> 189, 'Age > 189 is not possible');
+console.error('this is an error');
