cefc2f133d06fb590ec620611fdb7ff72f68194c
alok <alok60541@gmail.com>
2022-11-29 22:32:05 +0530

    time to execute fn

--- 29_nov_2022.js
+++ 29_nov_2022.js
@@ -36,9 +36,24 @@ console.log(removeFromLeft([1, 2, 3], 1));
 console.log(removeFromLeft([1, 2, 3], 2));
 console.log(removeFromLeft([1, 2, 3], 4));
 
-  // Output
+// Output
 //  [2,3]
 // [2,3]
 // [3]
 // []
+
+
+// Q4. find time taken by a function to execute
+const time_taken = callback => {
+    const result = callback();
+    return result;
+};
+console.log("Time taken: " + time_taken(() => Math.pow(2, 10)) + " ms");
+console.log("Time taken: " + time_taken(() => Math.sqrt(225)) + " ms");
+console.log("Time taken: " + time_taken(() => Math.sqrt((5 * 5) + (6 * 6))) + " ms");
+
+// Output
+// Time taken: 1024 ms
+// Time taken: 15 ms
+// Time taken: 7.810249675906654 ms
 
