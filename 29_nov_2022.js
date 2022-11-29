cc65b0564043647b6e51bc8b49687007b72a34b7
alok <alok60541@gmail.com>
2022-11-29 22:21:31 +0530

    odd/even with values

--- 29_nov_2022.js
+++ 29_nov_2022.js
@@ -7,3 +7,22 @@ var is_array = function (input) {
 };
 console.log(is_array('w3resource'));  //false
 console.log(is_array([1, 2, 4, 0]));    // true
+
+// Q2. output odd/even with values in js
+for (var x = 0; x <= 15; x++) {
+    if (x === 0) {
+        console.log(x + " is even");
+    }
+    else if (x % 2 === 0) {
+        console.log(x + " is even");
+    }
+    else {
+        console.log(x + " is odd");
+    }
+}
+// Output
+// 0 is even
+// 1 is odd
+// 2 is even
+// 3 is odd  ......
+
